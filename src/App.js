26976f9154a9bd13f6029b1672d951b60d0a5592
박재현 <pjh@yisistory.com>
2022-11-11 21:29:02 +0900

    최초 commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import requests from './api/requests';
+import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Row from './components/Row';
+import DetailPage from './pages/DetailPage';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
 
-function App() {
+const Layout = () => {
 	return (
-		<div className="App">
+		<div>
 			<Nav />
-			<Banner />
 
-			<Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow	/>
-			
-			<Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rate" id="TR" fetchUrl={requests.fetchTopRated} />
-			<Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" id="HM" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies} />
-			<Row title="Documentaries" id="DM" fetchUrl={requests.fetchDocumentaries} />
+			<Outlet />
 
-			<Footer></Footer>
+			<Footer />
+		</div>
+	)
+}
+
+const App = () => {
+	return (
+		<div className='app'>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<MainPage />} />
+					<Route path=':movieId' element={<DetailPage />} />
+					<Route path='search' element={<SearchPage />} />
+				</Route>
+			</Routes>
 		</div>
 	);
 }
